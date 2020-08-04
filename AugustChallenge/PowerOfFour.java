8894289803f1225b1826bcac327d6a69978c38db
Devanshi Khare <66879183+devanshi3107@users.noreply.github.com>
2020-08-04 22:43:35 +0530

    Create PowerOfFour.java

---
+++ AugustChallenge/PowerOfFour.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        double val=Math.log(num)/Math.log(4);
+        if(val%1==0)
+            return true;
+        else return false;
+    }
+}
+
+/***********
+O(1) time complexity
+**************/
