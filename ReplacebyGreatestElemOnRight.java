8ff3674a1155619cb48666bdf9f5de188635e7d9
Devanshi Khare <66879183+devanshi3107@users.noreply.github.com>
2020-08-09 22:40:15 +0530

    Create ReplacebyGreatestElemOnRight.java

---
+++ ReplacebyGreatestElemOnRight.java
@@ -0,0 +1,19 @@
+/*******************
+Input: arr = [17,18,5,4,6,1]
+Output: [18,6,6,6,1,-1]
+********************/
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n=arr.length;
+        int max=arr[n-1];
+        arr[n-1]=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            int temp=arr[i];
+            arr[i]=max;
+            if(temp>max)
+                max=temp;
+        }
+        return arr;
+    }
+}
