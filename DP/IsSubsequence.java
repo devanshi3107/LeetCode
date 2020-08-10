e52be7526cf05ae98379324836a1b43b4b3ac80f
Devanshi Khare <66879183+devanshi3107@users.noreply.github.com>
2020-08-11 01:10:42 +0530

    Create IsSubsequence.java

---
+++ DP/IsSubsequence.java
@@ -0,0 +1,35 @@
+/****************************
+Input: s = "abc", t = "ahbgdc"
+Output: true
+*****************************/
+
+class Solution {
+    public boolean isSubsequence(String X, String Y) {
+        int m=X.length();
+        int n=Y.length();
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0|| j==0)
+                    dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<m+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                if(X.charAt(i-1)==Y.charAt(j-1))
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        if(X.length()==dp[m][n])
+            return true;
+        else
+            return false;
+        
+    }
+}
