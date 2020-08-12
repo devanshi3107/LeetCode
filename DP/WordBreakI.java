32f0d012787e8c7cee14d3d955b39cbbf398645d
Devanshi Khare <66879183+devanshi3107@users.noreply.github.com>
2020-08-12 19:52:23 +0530

    Create WordBreakI.java

---
+++ DP/WordBreakI.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n=s.length();
+        boolean dp[]=new boolean[n+1];
+        dp[0]=true;
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(dp[j]==true && wordDict.contains(s.substring(j,i)))
+                    dp[i]=true;
+            }
+        }
+        return dp[n];
+    }
+}
